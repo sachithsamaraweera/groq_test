5005fe1287707a84861c38776519a0eb1c70d546
sachithsamaraweera <sachithsamaraweera@gmail.com>
2024-02-27 10:14:50 +0530

    test

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables.config import RunnableConfig
+from langchain_core.runnables.base import Runnable
 from langchain_groq import ChatGroq
 load_dotenv()
 
 
-chat = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
-system = "You are a presales engineer who has a experience in telco domain over 15 years. you can explain any technical details to the customer."
-human = "{text}"
-prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+import chainlit as cl
 
-chain = prompt | chat
 
-while True:
-    userinp = input(">>>")
-    result = chain.invoke({"text": userinp})
-    print(result.content)
+@cl.on_chat_start
+async def on_chat_start():
+    model = ChatGroq(temperature=0, model_name="mixtral-8x7b-32768")
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a helpful assistant developed by AI Engineering team at JIT",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable")  # type: Runnable
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
